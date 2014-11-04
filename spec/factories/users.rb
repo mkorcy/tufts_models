b6277d2e21cd36d5a8a90a48c4ab35edf7539ddb
Mike Korcynski <mkorcy@gmail.com>
2014-11-04 14:20:05 -0500

    modifies tests to use username instead of email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
-    sequence :email do |n|
-      "person#{User.count}_#{n}@example.com"
+    sequence :username do |n|
+      "person#{User.count}_#{n}"
     end
     password 'password'
 
     factory :admin do
       roles { [Role.where(name: 'admin').first_or_create] }
     end
-  end
+  end 
 end
